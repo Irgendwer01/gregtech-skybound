952dd447a11aec006c8c79ceb69e918c5ff30355
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2024-09-10 23:21:35 +0200

    Make block breaker take 1 iron ingot and move its quest earlier
    Also make flint tools quest depend on hammer quest

--- groovy/postInit/openblocks.groovy
+++ groovy/postInit/openblocks.groovy
@@ -73,7 +73,7 @@ for (def elevator_rotating : ore('elevator_rotating')) {
 
 crafting.addShaped('block_breaker', item('openblocks:block_breaker'), [
  [metaitem('plateIron'), metaitem('plateIron'), item('minecraft:cobblestone')],
- [metaitem('toolHeadBuzzSawWroughtIron'), item('minecraft:redstone'), item('minecraft:cobblestone')],
+ [metaitem('toolHeadBuzzSawWroughtIron'), item('minecraft:iron_ingot'), item('minecraft:cobblestone')],
  [metaitem('plateIron'), metaitem('plateIron'), item('minecraft:cobblestone')]])
  
 crafting.addShaped('openblocks_elevator', item('openblocks:elevator'), [
